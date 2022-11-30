e35d4601dccf1d20bba7a04773031173d041c81e
Ben Plate <bplate9583@gmail.com>
2022-11-30 11:35:57 -0800

    fix className react issue

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -9,10 +9,10 @@ import ScatterChart2 from "./Charts/ScatterChart2";
 function Contact() {
   return (
     <div className="contact">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <h1 class="font-weight-light">World University Rankings Visualized</h1>
+      <div className="container">
+        <div className="row align-items-center my-5">
+          <div className="col-lg-7">
+            <h1 className="font-weight-light">World University Rankings Visualized</h1>
             <p>
               Below are charts depicting subsets of the 
               <a href="https://www.topuniversities.com/university-rankings/world-university-rankings/2023" style={{color: 'blue'}}> QS World University Rankings 2023</a>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer class="py-3 fixed-bottom" style={{backgroundColor: '#576F72'}}>
-        <div class="container" style={{fontFamily: 'Roboto'}}>
-          <p class="m-0 text-center text-white">
+      <footer className="py-3 fixed-bottom" style={{backgroundColor: '#576F72'}}>
+        <div className="container" style={{fontFamily: 'Roboto'}}>
+          <p className="m-0 text-center text-white">
             CSE412 De Luca - Fall 2022
           </p>
         </div>
