3f7c4123a7871ced825894c8979016187d7cf767
Shra <shrayswarup@gmail.com>
2022-11-29 18:45:25 -0700

    up

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,6 @@ function About() {
           <div class="col-lg-7">
             <img class="img-fluid rounded mb-4 mb-lg-0"
             src={DeLuca} alt="test"/>
-            {/* <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            /> */}
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">About</h1>
@@ -23,10 +18,7 @@ function About() {
               This is achieved because the visuals rely on a database which can be updated yearly and as a result
               the summary statistics visualizations do not have to be edited at all assuming the ranking criteria remain the same.
               This project is locally hosted and uses a local PostgreSQL server and Javascript Plotly. By Javier Aguilar, Joseph Olakkengil, and Shray Swarup.
-
-
             </p>
-
           </div>
         </div>
       </div>
